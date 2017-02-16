6dab7bf7b91aa3d43359f3f1ca2d519184e7c29f
Travis <noreply@travis-ci.org>
2017-02-16 14:44:00 +0000

    Deploy to GitHub Pages

---
+++ implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["geo"] = ["impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a> for <a class='struct' href='geo/struct.Coordinate.html' title='struct geo::Coordinate'>Coordinate</a>&lt;T&gt; <span class='where fmt-newline'>where T: <a class='trait' href='num_traits/float/trait.Float.html' title='trait num_traits::float::Float'>Float</a></span>","impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a> for <a class='struct' href='geo/struct.Bbox.html' title='struct geo::Bbox'>Bbox</a>&lt;T&gt; <span class='where fmt-newline'>where T: <a class='trait' href='num_traits/float/trait.Float.html' title='trait num_traits::float::Float'>Float</a></span>","impl&lt;T:&nbsp;<a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a>&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='trait core::marker::Copy'>Copy</a> for <a class='struct' href='geo/struct.Point.html' title='struct geo::Point'>Point</a>&lt;T&gt; <span class='where fmt-newline'>where T: <a class='trait' href='num_traits/float/trait.Float.html' title='trait num_traits::float::Float'>Float</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.AddAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["geo"] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='trait core::ops::AddAssign'>AddAssign</a> for <a class='struct' href='geo/struct.Bbox.html' title='struct geo::Bbox'>Bbox</a>&lt;T&gt; <span class='where fmt-newline'>where T: <a class='trait' href='num_traits/float/trait.Float.html' title='trait num_traits::float::Float'>Float</a> + <a class='trait' href='num_traits/cast/trait.ToPrimitive.html' title='trait num_traits::cast::ToPrimitive'>ToPrimitive</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
